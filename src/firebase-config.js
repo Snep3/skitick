ebf76b8ca7a7fe90d8db60fdd1f8fe69f0a6e67e
Snaps3ot <gavrielsha359@gmail.com>
2025-03-31 18:09:46 +0300

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,12 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBEMB91q8LimediUyahDQAoFMc73E1WcB8",
-    authDomain: "studyproject-e81dc.firebaseapp.com",
-    projectId: "studyproject-e81dc",
-    storageBucket: "studyproject-e81dc.firebasestorage.app",
-    messagingSenderId: "689819119211",
-    appId: "1:689819119211:web:0ba0c0d3003aaeaf8ecb2e"
+
   };
 
 // Initialize Firebase
